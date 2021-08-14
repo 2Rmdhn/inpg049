1269f714e1614b22277a2a094f3346292e5857d8
2Rmdhn <dwi.ramadhian@gmail.com>
2021-08-15 02:22:06 +0700

    Tugas 2 Update

--- Tugas 2/index.js
+++ Tugas 2/index.js
@@ -6,15 +6,20 @@ function showSection(){
     document.getElementById("editform").style.display = "block"
 }
 
-function saveForm(){
-    var nama = document.getElementById(Nama).value;
-    var role = document.getElementById(Role).value;
-    var availability = document.getElementById(Availability).value;
-    var usia = document.getElementById(Usia).value;
-    var lokasi = document.getElementById(Lokasi).value;
-    var pengalaman = documen.getElementById(Pengalaman).value;
-    var email = documen.getElementById(Email).value;
+function showForm(){
+    var nama = document.getElementById("Nama").value
+    var role = document.getElementById("Role").value
+    var availability = document.getElementById("Availability").value
+    var usia = document.getElementById("Usia").value
+    var lokasi = document.getElementById("Lokasi").value
+    var pengalaman = document.getElementById("Pengalaman").value
+    var email = document.getElementById("Email").value
 
-    document.getElementById(Newnama).innerHTML = nama;
-    document.getElementById(Newrole).innerHTML = role;
+    document.getElementById("Newnama").innerHTML = nama
+    document.getElementById("Newrole").innerHTML = role
+    document.getElementById("Newava").innerHTML = availability
+    document.getElementById("Newusia").innerHTML = usia
+    document.getElementById("Newlokasi").innerHTML = lokasi
+    document.getElementById("Newexp").innerHTML = pengalaman
+    document.getElementById("Newemail").innerHTML = email
 }
